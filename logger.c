d4c035b4d05b216eb6e519fbe7c2a0dd662f6570
JoelJa835 <joeljani71@gmail.com>
2022-11-28 13:47:51 +0200

    finished fwrite too. Both of them work perfectly as far as i have tested.Also cleaned logger.c and added comments

--- logger.c
+++ logger.c
@@ -9,6 +9,7 @@
 #include <sys/stat.h>
 #include <openssl/md5.h>
 #include <errno.h>
+#include <linux/limits.h>
 
 #ifndef LOGFILE
 #define LOGFILE "//home/jogopogo/Projects/Archive/src_corpus/file_logging.log"   
@@ -45,118 +46,138 @@ char * getTimeAndDate(){
 }
 
 char * calculate_file_md5(FILE *original_fopen_ret) {
-	unsigned char c[MD5_DIGEST_LENGTH];
-	int i;
-	MD5_CTX mdContext;
-	int bytes;
-	unsigned char data[1024];
 	char *filemd5 = (char*) malloc(33 *sizeof(char));
 
-	MD5_Init (&mdContext);
+	// create file fingerprint
+	unsigned char fingerprint[MD5_DIGEST_LENGTH];
+	int length = 0;
 
-	while ((bytes = fread (data, 1, 1024, original_fopen_ret)) != 0)
+	int current_pos = ftell( original_fopen_ret );
+	fseek( original_fopen_ret , 0 , SEEK_END );
+	length = ftell( original_fopen_ret );
 
-	MD5_Update (&mdContext, data, bytes);
+	// read file
+	fseek( original_fopen_ret , 0 , SEEK_SET );
+	char buf[length];
+	fread( buf , 1 , length , original_fopen_ret );
 
-	MD5_Final (c,&mdContext);
-
-	for(i = 0; i < MD5_DIGEST_LENGTH; i++) {
-		sprintf(&filemd5[i*2], "%02x", (unsigned int)c[i]);
+	// reset seek
+	fseek( original_fopen_ret , 0 , current_pos );
+	
+	// create fingerprint
+	MD5_CTX mdContext;
+	MD5_Init( &mdContext );
+	MD5_Update( &mdContext , buf , length );
+	MD5_Final( fingerprint , &mdContext );
+	for(int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+		sprintf(&filemd5[i*2], "%02x", (unsigned int)fingerprint[i]);
 	}
-
-	//printf("calculated md5:%s ", filemd5);
 	return filemd5;
 }
 
+char * recover_filename(FILE * f) {
+  int fd;
+  char fd_path[0xFFF];
+  char * filename = malloc(0xFFF);
+  ssize_t n;
+
+  fd = fileno(f);
+  sprintf(fd_path, "/proc/self/fd/%d", fd);
+  n = readlink(fd_path, filename, 0xFFF);
+  if (n < 0)
+      return NULL;
+  filename[n] = '\0';
+  return filename;
+}
 
 
-
-
-
-FILE *
-fopen(const char *path, const char *mode) 
+FILE* fopen( const char* path , const char* mode ) 
 {
-	char *filemd5 = (char*) malloc(33 *sizeof(char));
+	//Get UID
 	uid_t user_id = getuid();
-	int isDeniedFlag = 0;
+
+	//Get access type
 	int acc_type = 0;
-	
-	fprintf(stdout,"Went into custom fopen\n");
-	//Check if the file already exists
-	//Note both stat and access have the same TOCTOU vulnerability.So using x mode while opening would be prefered to avoid the TOCTOU.
-	//However it exceeds the boundaries of the project, so we continue with access.
 	if(access(path,F_OK)==0){
-		//File exists
-		if(*mode == 'r'|| strcmp(mode, "r+") == 0 || strcmp(mode, "rb") == 0 || strcmp(mode, "r+b") == 0 || strcmp(mode, "rb+") == 0)
-			acc_type = 1;
-		//If the file exists and we open it with write permissions, its content are cleared, so the file is considered a new file
-		else if( *mode == 'w' || strcmp(mode, "w+") == 0 || strcmp(mode, "wb") == 0 || strcmp(mode, "w+b") == 0 || strcmp(mode, "wb+") == 0)
-			acc_type = 3;
-		else if( *mode == 'a' || strcmp(mode, "a+") == 0 || strcmp(mode, "ab") == 0 || strcmp(mode, "a+b") == 0 || strcmp(mode, "ab+") == 0)					
-			acc_type = 2;
-		else
-			acc_type = -1;
+	//File exists
+	if(*mode == 'r'|| strcmp(mode, "r+") == 0 || strcmp(mode, "rb") == 0 || strcmp(mode, "r+b") == 0 || strcmp(mode, "rb+") == 0)
+		acc_type = 1;
+	//If the file exists and we open it with write permissions, its content are cleared, so the file is considered a new file
+	else if( *mode == 'w' || strcmp(mode, "w+") == 0 || strcmp(mode, "wb") == 0 || strcmp(mode, "w+b") == 0 || strcmp(mode, "wb+") == 0)
+		acc_type = 3;
+	else if( *mode == 'a' || strcmp(mode, "a+") == 0 || strcmp(mode, "ab") == 0 || strcmp(mode, "a+b") == 0 || strcmp(mode, "ab+") == 0)					
+		acc_type = 2;
+	else
+		acc_type = -1;
 	}
 	else{
-		//If the file doesnt already exist and we try to open with write permissions, the file will be created.Same with append permissions
-		if( *mode == 'w' || strcmp(mode, "w+") == 0 || strcmp(mode, "wb") == 0 || strcmp(mode, "w+b") == 0 || strcmp(mode, "wb+") == 0)
-			acc_type = 0;
-		else if( *mode == 'a' || strcmp(mode, "a+") == 0 || strcmp(mode, "ab") == 0 || strcmp(mode, "a+b") == 0 || strcmp(mode, "ab+") == 0)					
-			acc_type = 0;
-		else
-			acc_type = -1;
+	//If the file doesnt already exist and we try to open with write permissions, the file will be created.Same with append permissions
+	if( *mode == 'w' || strcmp(mode, "w+") == 0 || strcmp(mode, "wb") == 0 || strcmp(mode, "w+b") == 0 || strcmp(mode, "wb+") == 0)
+		acc_type = 0;
+	else if( *mode == 'a' || strcmp(mode, "a+") == 0 || strcmp(mode, "ab") == 0 || strcmp(mode, "a+b") == 0 || strcmp(mode, "ab+") == 0)					
+		acc_type = 0;
+	else
+		acc_type = -1;
 	}
-	
-  	FILE *original_fopen_ret;
-	FILE *(*original_fopen)(const char*, const char*);
-	/* call the original fopen function */
-	original_fopen = dlsym(RTLD_NEXT, "fopen");
-	original_fopen_ret = (*original_fopen)(path, mode);
 
-	if(!original_fopen_ret && (errno == EACCES || errno == EBADF || errno == EPERM))
+	//Call the original fopen function
+	FILE* original_fopen_ret;
+	FILE* (*original_fopen)( const char* , const char* );
+	original_fopen = dlsym( RTLD_NEXT , "fopen" );
+	original_fopen_ret = (*original_fopen)( path , mode );
+
+	//Check if action denied
+	int isDeniedFlag = 0;
+	if( !original_fopen_ret && (errno == EACCES || errno == EBADF || errno == EPERM) )
 		isDeniedFlag = 1;
 
+	//Get actual path
 	char * actualpath = realpath(path,NULL);
-	if (actualpath == NULL)
-		printf("Errno: %d\n", errno);
 
-	if(isDeniedFlag){
-		original_fopen_ret = (*original_fopen)( path , "r" );
-		filemd5 = calculate_file_md5(original_fopen_ret);
-		fclose(original_fopen_ret);
-	}
-	else
-		filemd5 = calculate_file_md5(original_fopen_ret);
-	
+	//Open log file in append mode
 	FILE* log = (*original_fopen)( "file_logging.log" , "a" );
-	chmod( "file_logging.log" , 0777 );
+	//Change permissions
+	chmod( "file_logging.log"  , 0777 );
 
+	//Write to file
+	fprintf( log , "%d %d %d %s %s %s" , user_id , acc_type , isDeniedFlag , actualpath,calculate_file_md5(original_fopen_ret),getTimeAndDate());
 
-	fprintf(stdout,"Before writing\n");
-	fprintf(log,"%d %s %d %d %s %s",user_id,actualpath,acc_type,isDeniedFlag,filemd5,getTimeAndDate());
-           
 	fclose(log);
-	
 
-	free(actualpath);
 	return original_fopen_ret;
 }
 
 
-size_t 
-fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) 
+size_t fwrite( const void* ptr , size_t size , size_t nmemb , FILE* stream ) 
 {
+	//Get UID
+	uid_t user_id = getuid();
 
-	size_t original_fwrite_ret;
-	size_t (*original_fwrite)(const void*, size_t, size_t, FILE*);
+	//Get access type
+	int acc_type = 2;
 
 	/* call the original fwrite function */
-	original_fwrite = dlsym(RTLD_NEXT, "fwrite");
-	original_fwrite_ret = (*original_fwrite)(ptr, size, nmemb, stream);
+	size_t original_fwrite_ret;
+	size_t (*original_fwrite)( const void* , size_t , size_t , FILE* );
+	original_fwrite = dlsym( RTLD_NEXT, "fwrite" );
+	original_fwrite_ret = (*original_fwrite)( ptr , size , nmemb , stream );
 
-	
+	//Check if action denied
+	int isDeniedFlag = 0;
+	if( !original_fwrite_ret && (errno == EACCES || errno == EBADF || errno == EPERM) )
+		isDeniedFlag = 1;
 
+	//Open log file in append mode
+	FILE* (*original_fopen)( const char* , const char* );
+	original_fopen = dlsym( RTLD_NEXT , "fopen" );
+	FILE* log = (*original_fopen)( "file_logging.log" , "a" );
+	//Change permissions
+	chmod( "file_logging.log"  , 0777 );
 
+	//Write to log file
+	fprintf( log , "%d %d %d %s %s %s" , user_id , acc_type , isDeniedFlag ,recover_filename(stream),calculate_file_md5(stream),getTimeAndDate());
+
+	fclose(log);
 
 	return original_fwrite_ret;
 }
--- test_aclog.c
+++ test_aclog.c
@@ -6,35 +6,51 @@ int main()
 	int i;
 	size_t bytes;
 	FILE *file;
-	char filenames[10][7] = {"file_0", "file_1", 
-			"file_2", "file_3", "file_4",
-			"file_5", "file_6", "file_7", 		
-			"file_8", "file_9"};
+	// char filenames[10][7] = {"file_0", "file_1", 
+	// 		"file_2", "file_3", "file_4",
+	// 		"file_5", "file_6", "file_7", 		
+	// 		"file_8", "file_9"};
+	char filenames[2][7] = { "file_0" , "file_1"  };
 
 
 	/* example source code */
 
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < 2; i++) {
 
 		file = fopen(filenames[i], "w+");
 		if (file == NULL) 
 			printf("fopen error\n");
 		else {
-			printf("Here\n");
-			//bytes = fwrite(filenames[i], strlen(filenames[i]), 1, file);
+			printf("Here %s\n",filenames[i]);
 			fclose(file);
 		}
 
 	}
 
-	file = fopen(filenames[0], "r");
+	file = fopen(filenames[0], "r"); // file_0 open once
+	if (file == NULL) 
+		printf("fopen error\n");
+	else
+	{	// all should fail because file was opened with wrong mode
+		bytes = fwrite(filenames[0], strlen(filenames[0]), 1, file); // file modification 5 times
+		bytes = fwrite(filenames[0], strlen(filenames[0]), 1, file);
+		bytes = fwrite(filenames[0], strlen(filenames[0]), 1, file);
+		bytes = fwrite(filenames[0], strlen(filenames[0]), 1, file);
+		bytes = fwrite(filenames[0], strlen(filenames[0]), 1, file);
+		fclose(file);
+	}
 
-
-	/* add your code here */
-	/* ... */
-	/* ... */
-	/* ... */
-	/* ... */
-
-
+	file = fopen(filenames[1], "r+"); // file_1 open once
+	if (file == NULL) 
+		printf("fopen error\n");
+	else
+	{	// all should work if user has write rights
+		bytes = fwrite(filenames[1], strlen(filenames[1]), 1, file); // file modification 5 times
+		bytes = fwrite(filenames[1], strlen(filenames[1]), 1, file);
+		bytes = fwrite(filenames[1], strlen(filenames[1]), 1, file);
+		bytes = fwrite(filenames[1], strlen(filenames[1]), 1, file);
+		bytes = fwrite(filenames[1], strlen(filenames[1]), 1, file);
+		fclose(file);
+	}
+	//fclose(file);
 }
